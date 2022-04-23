fe932e3d32524260270f624df6df5d69ae641a59
Edith <eduardsan627@gmail.com>
2022-04-22 20:17:41 -0500

    terminacion de extra, odie medio mundo

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,14 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+
+  var nuevaMatriz = []
+  
+  for (var clave in objeto){
+    nuevaMatriz.push([clave, objeto[clave]]);
+  };
+
+  return nuevaMatriz;
 }
 
 
@@ -18,6 +26,18 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+
+  var contadorPalabras = {}
+
+  for (var i = 0; i < string.length; i++){
+    if (contadorPalabras.hasOwnProperty(string[i])){
+      contadorPalabras[string[i]] += 1
+    }else {
+      contadorPalabras[string[i]] = 1
+    }
+  }
+
+  return contadorPalabras;
 }
 
 
@@ -26,8 +46,22 @@ function capToFront(s) {
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+  var mayúsculas = '';
+  var minusculas = '';
+
+  for (i = 0; i < s.length; i++){
+    if(s[i] === s[i].toUpperCase()){
+      mayúsculas += s[i];
+    }else {
+      minusculas += s[i];
+    };
+  };
+
+  return mayúsculas + minusculas;
 }
 
+//str = hola como estas
+//return = aloh omoc satse
 
 function asAmirror(str) {
   //La función recibe una frase. 
@@ -35,6 +69,14 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
+
+  var espejo = str.split(' ')
+
+  var palabrasInvertidad = espejo.map(function (elemento){
+    return elemento.split('').reverse().join('')
+  })
+  
+  return palabrasInvertidad.join(' ');
 } 
 
 
@@ -43,6 +85,14 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+  var pasarNumero = numero.toString();
+  var capicua = pasarNumero.split('').reverse().join('');
+
+  if (capicua === pasarNumero){
+    return 'Es capicua';
+  }else {
+    return 'No es capicua';
+  }
 }
 
 
@@ -50,6 +100,17 @@ function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+  var eliminar = '';
+
+  for(i = 0; i < cadena.length; i++){
+    if(cadena[i] === 'a' || cadena[i] === 'b' || cadena[i] === 'c'){
+      continue;
+    } else {
+      eliminar += cadena[i]
+    }
+  }
+
+  return eliminar;
 }
 
 
@@ -57,8 +118,25 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
-}
+  var cambio = true;
 
+  while (cambio){
+
+    cambio = false;
+
+    for (var i = 0; i < arr.length -1; i++){
+      if(arr[i].length > arr[i+1].length){
+        var aux = arr[i];
+        arr[i] = arr [i+1];
+        arr[i+1] = aux;
+        cambio = true;
+      }
+    }
+
+  }
+
+  return arr;
+}
 
 function buscoInterseccion(arreglo1, arreglo2){
   //Existen dos arrays, cada uno con 5 números. A partir de ello, escribir una función que permita 
@@ -66,6 +144,16 @@ function buscoInterseccion(arreglo1, arreglo2){
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
   //Escribe tu código aquí  
+  var intersección = [];
+  for (var i = 0; i < arreglo1.length; i++){
+    for (var k = 0; k < arreglo2.length; k++){
+      if (arreglo1[i] === arreglo2[k]){
+        intersección.push(arreglo2[k]);
+      }
+    }
+  } 
+
+  return intersección;
 }
 
 
